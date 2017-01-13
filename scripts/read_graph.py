39c6fef5a27971fa5a55d237d010f0d4eae238b2
joao.carabetta@gmail.com <joao.carabetta@gmail.com>
2017-01-13 15:08:09 -0200

    read_graph added

---
+++ scripts/read_graph.py
@@ -0,0 +1,23 @@
+import ast
+import networkx as nx
+
+def read(path):
+
+    MG = nx.read_gexf(path)
+
+    for node in MG.nodes(data=True):
+
+        for key, value in MG.edge[node[0]].items():
+            for k, v in value.items():
+                if type(v['attr']) == str:
+                    MG.edge[node[0]][key][k]['attr'] = ast.literal_eval(v['attr'])
+
+    for node in MG.nodes(data=True):
+        if type(MG.node[node[0]]['location']) == str:
+            pass
+            # MG.node[node[0]]['location'] = ast.literal_eval(node[1]['location'])
+        if len(node[1]) == 3:
+            if type(MG.node[node[0]]['viagens']) == str:
+                MG.node[node[0]]['viagens'] = ast.literal_eval(MG.node[node[0]]['viagens'])
+
+    return MG
