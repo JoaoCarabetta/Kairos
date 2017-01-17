ce94177ec30884387af87baad710d18b25273332
joao.carabetta@gmail.com <joao.carabetta@gmail.com>
2017-01-17 13:46:05 -0200

    começo da isocrona

--- scripts/read_graph.py
+++ scripts/read_graph.py
@@ -6,16 +6,14 @@ def read(path):
     MG = nx.read_gexf(path)
 
     for node in MG.nodes(data=True):
-
         for key, value in MG.edge[node[0]].items():
             for k, v in value.items():
                 if type(v['attr']) == str:
                     MG.edge[node[0]][key][k]['attr'] = ast.literal_eval(v['attr'])
 
     for node in MG.nodes(data=True):
-        if type(MG.node[node[0]]['location']) == str:
-            pass
-            # MG.node[node[0]]['location'] = ast.literal_eval(node[1]['location'])
+        if isinstance(MG.node[node[0]]['location'], str):
+            MG.node[node[0]]['location'] = ast.literal_eval(MG.node[node[0]]['location'])
         if len(node[1]) == 3:
             if type(MG.node[node[0]]['viagens']) == str:
                 MG.node[node[0]]['viagens'] = ast.literal_eval(MG.node[node[0]]['viagens'])
